a229e5af1e37328e9462de09e2bcfe998068022e
warren <warrenzshen@gmail.com>
2015-04-15 18:35:36 -0700

    Create person model with devise

---
+++ test/models/person_test.rb
@@ -0,0 +1,29 @@
+# == Schema Information
+#
+# Table name: people
+#
+#  id                     :integer          not null, primary key
+#  first_name             :string           default(""), not null
+#  last_name              :string           default(""), not null
+#  birthday               :date
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default("0"), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string
+#  last_sign_in_ip        :string
+#  created_at             :datetime
+#  updated_at             :datetime
+#
+
+require 'test_helper'
+
+class PersonTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

62528c5f6e020610982cfed7d739d0f43b91d957
warren <warrenzshen@gmail.com>
2015-04-16 20:21:15 -0700

    Finish variation of header

--- app/assets/javascripts/components/header.jsx
+++ app/assets/javascripts/components/header.jsx
@@ -16,17 +16,26 @@ var Header = React.createClass({
       "header": true,
       "header-colored": this.props.isColored,
     });
+    var brandClass = merge({
+      "header-brand": true,
+      "primary-color": this.props.isColored,
+    });
+    var loginClass = merge({
+      "header-login": true,
+      "primary-color": this.props.isColored,
+    });
+
     return (
       <div className={headerClass}>
         <div className="header-left">
           <div className="vertical-anchor"></div>
-          <a className="header-brand-text">
+          <a className={brandClass}>
             Kazoku
           </a>
         </div>
         <div className="header-right">
           <div className="vertical-anchor"></div>
-          <a className="header-login-button" href={this.props.loginPath}>
+          <a className={loginClass} href={this.props.loginPath}>
             Signup/Login
           </a>
         </div>

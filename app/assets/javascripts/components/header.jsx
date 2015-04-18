80f862335a424a566a32e75fd76acd9c335d69ab
warren <warrenzshen@gmail.com>
2015-04-18 15:42:48 -0700

    Render first comopnent in another component

--- app/assets/javascripts/components/header.jsx
+++ app/assets/javascripts/components/header.jsx
@@ -14,6 +14,28 @@ var Header = React.createClass({
     isColored:     true,
   },
 
+  renderProfile: function() {
+    var merge = React.addons.classSet;
+    var loginClass = merge({
+      "header-login": true,
+      "header-login-colored": this.props.isColored,
+    });
+    if (this.props.currentPerson === null) {
+      return (
+        <a className={loginClass} href={this.props.loginPath}>
+          Signup/Login
+        </a>
+      );
+    } else {
+      return (
+        <Clickable
+          style={loginClass}
+          path={this.props.homePath}
+          content={"Your Profile"} />
+      );
+    }
+  },
+
   render: function() {
     var merge = React.addons.classSet;
     var headerClass = merge({
@@ -24,11 +46,6 @@ var Header = React.createClass({
       "header-brand": true,
       "header-brand-colored": this.props.isColored,
     });
-    var loginClass = merge({
-      "header-login": true,
-      "header-login-colored": this.props.isColored,
-    });
-    console.log(this.props.currentPerson);
     return (
       <div className={headerClass}>
         <div className="header-left">
@@ -39,9 +56,7 @@ var Header = React.createClass({
         </div>
         <div className="header-right">
           <div className="vertical-anchor"></div>
-          <a className={loginClass} href={this.props.loginPath}>
-            Signup/Login
-          </a>
+          {this.renderProfile()}
         </div>
       </div>
     );

590d791bee194826481153f4467b7c8fca5f6472
warren <warrenzshen@gmail.com>
2015-04-22 20:42:33 -0700

    Fix styling and add third action

--- app/assets/javascripts/components/home_actions.jsx
+++ app/assets/javascripts/components/home_actions.jsx
@@ -4,24 +4,40 @@ var HomeActions = React.createClass({
     return (
       <div className="home-actions">
         <div className="home-action">
-          <Clickable
-            path={Routes.users.index}
-            style={"home-action-image"}
-            source={"http://www.ruinart.com/img/content/articles/13_Portrait%20HVDS%20-%20HD.jpg"} />
+          <div className="home-action-frame">
+            <Clickable
+              path={Routes.users.index}
+              style={"home-action-image"}
+              source={"http://www.ruinart.com/img/content/articles/13_Portrait%20HVDS%20-%20HD.jpg"} />
+          </div>
           <Clickable
             path={Routes.users.index}
             style={"home-action-clickable"}
             content={"People"} />
         </div>
         <div className="home-action">
-          <Clickable
-            path={Routes.families.index}
-            style={"home-action-image"}
-            source={"http://www.vlblog.net/blogimage/anwan01.jpg"} />
+          <div className="home-action-frame">
+            <Clickable
+              path={Routes.families.index}
+              style={"home-action-image"}
+              source={"http://www.vlblog.net/blogimage/anwan01.jpg"} />
+          </div>
           <Clickable
             path={Routes.families.index}
             style={"home-action-clickable"}
             content={"Families"} />
+        </div>
+        <div className="home-action">
+          <div className="home-action-frame">
+            <Clickable
+              path={Routes.events.index}
+              style={"home-action-image"}
+              source={"http://cdn2.hellogiggles.com/wp-content/uploads/2015/04/09/Bassnectar_Live_at_Coachella_Wknd_2.jpg"} />
+          </div>
+          <Clickable
+            path={Routes.events.index}
+            style={"home-action-clickable"}
+            content={"Events"} />
         </div>
       </div>
     );

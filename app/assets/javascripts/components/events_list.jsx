7a6d0feb7f758c41edb71d06b7bd7f349452ef03
warren <warrenzshen@gmail.com>
2015-04-23 17:40:46 -0700

    Create events list component

---
+++ app/assets/javascripts/components/events_list.jsx
@@ -0,0 +1,30 @@
+var EventsList = React.createClass({
+
+  propTypes: {
+    events: React.PropTypes.array.isRequired,
+  },
+
+  getDefaultProps: function() {
+    return {
+      events: [],
+    };
+  },
+
+  renderEvent: function(event) {
+    return (
+      <Event key={event.id} event={event} />
+    );
+  },
+
+  renderEvents: function() {
+    return this.props.events.map(this.renderEvent);
+  },
+
+  render: function() {
+    return (
+      <div className="general-list">
+        {this.renderEvents()};
+      </div>
+    );
+  }
+});

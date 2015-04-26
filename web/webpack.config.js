2362e038ca73bf2e150cfc45521ac56c35f5c2f1
warren <warrenzshen@gmail.com>
2015-04-26 16:36:03 -0700

    Some minor refactoring - now we have a node js host on 3333

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -11,7 +11,8 @@ module.exports = {
     filename: "bundle.js",
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
   ],
   resolve: {
     extensions: ["", ".js"]

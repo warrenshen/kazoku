a0abcbe03c8a658b62efdcb6b941482d6dc4b370
warren <warrenzshen@gmail.com>
2015-04-26 16:47:01 -0700

    Final basic preferences for now

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -4,14 +4,17 @@ var server = require('webpack-dev-server');
 var config = require('./webpack.config');
 
 gulp.task("serve", function() {
-  new server(webpack(config), { hot: true })
-    .listen(3333, "localhost", function (err, result) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("Listening at localhost:3333");
-      }
-    });
+  new server(webpack(config), {
+    hot: true,
+    stats: { colors: true}
+  })
+  .listen(3333, "localhost", function (err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Listening at localhost:3333");
+    }
+  });
 });
 
 gulp.task("default", ["serve"]);

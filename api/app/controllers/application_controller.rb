7f01e7722d3147093c3d307b2e5cd131f6987529
warren <warrenzshen@gmail.com>
2015-04-27 00:45:43 -0700

    Set up rails api gem - only api now

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,8 +1,5 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
   before_filter :authenticate_user!
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
 
   def authenticate_user!
     redirect_to login_path unless user_signed_in?

ecc4e58e029046c93ac702c0218a9f455e23853c
warren <warrenzshen@gmail.com>
2015-04-27 01:09:12 -0700

    Move all assets into their correct locations

--- web/app/components/header.jsx
+++ web/app/components/header.jsx
@@ -9,6 +9,15 @@ import Routes from "../constants/routes.js";
 
 class Header extends Component {
 
+  // send: function(type, path, arguments) {
+  //   var request = new XMLHttpRequest();
+  //   request.open(type, path);
+  //   request.setRequestHeader("Content-Type", "application/json");
+  //   request.setRequestHeader("X-CSRF-Token", $('meta[name="csrf-token"]').attr('content'));
+  //   request.send(JSON.stringify(arguments));
+  //   return request;
+  // }
+
   attemptLogout(event) {
     // var email = this.props.currentUser.email;
     // var request = Requester.send("delete", ApiRoutes.users.logout, {

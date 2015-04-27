21e70217ea6ed0be4d1b6c6262b3dd65903d2d0a
warren <warrenzshen@gmail.com>
2015-04-27 01:04:42 -0700

    Set up api error responses in application controller

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -4,15 +4,27 @@ class ApplicationController < ActionController::API
   respond_to :json
 
   def authenticate_user!
-    redirect_to login_path unless user_signed_in?
+    unauthorized_response unless user_signed_in?
   end
 
   def api_error_response(object=nil, message=nil, status=400)
     render json: ApiError.new(object, message), status: status
   end
 
+  def unauthorized_response
+    api_error_response(message: "Unauthorized", status: 401)
+  end
+
+  def not_found_response
+    api_error_response(message: "Not found", status: 404)
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url
+    unauthorized_response
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    not_found_response
   end
 
 end

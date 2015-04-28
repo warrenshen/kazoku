1b2ddd76fb2d67d782a1d32ce0f563ee6b40cf88
warren <warrenzshen@gmail.com>
2015-04-27 22:02:05 -0700

    Convert user component to person

---
+++ web/app/components/person.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Component from "../component.jsx";
+
+import Clickable from "./clickable.jsx";
+
+import Routes from "../constants/routes.js";
+
+
+class Person extends Component {
+
+  renderName: function() {
+    var person = this.props.person;
+    return person.first_name + " " + person.last_name;
+  }
+
+  renderImage: function() {
+    var person = this.props.person;
+    if (person.image_url.length) {
+      return (
+        <Clickable
+          route={Routes.people.index + "/" + person.id}
+          style={"user-block-image"}
+          source={person.image_url} />
+      );
+    }
+  }
+
+  render: function() {
+    return (
+      <div className="user-block">
+        {this.renderImage()}
+        <Clickable
+          route={Routes.people.index + "/" + this.props.person.id}
+          style={"user-block-name"}
+          content={this.renderName()} />
+      </div>
+    );
+  }
+}
+
+Person.propTypes = {
+  person: React.PropTypes.object.isRequired,
+}
+
+Person.defaultProps = {
+  person: null,
+}
+
+
+module.exports = Person;

8f4a47f4226b136a6ab04fdceb889eb717cd4570
warren <warrenzshen@gmail.com>
2015-04-27 17:56:07 -0700

    Preven incessant refreshing when going to home path

--- web/app/components/clickable.jsx
+++ web/app/components/clickable.jsx
@@ -7,6 +7,7 @@ class Clickable extends Component {
   handleClick() {
     event.stopPropagation();
     if (this.props.path !== "") {
+      event.preventDefault();
       // TODO: Figure out a way to do this without global scope.
       App.Router.navigate(this.props.path, true);
     } else if (this.props.action !== null) {

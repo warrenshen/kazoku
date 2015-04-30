22279f67fbbb33247ef378118988fe30d4b1cc9f
warren <warrenzshen@gmail.com>
2015-04-29 20:17:36 -0700

    Create family collection and model

--- web/app/models/family.js
+++ web/app/models/family.js
@@ -1,8 +1,38 @@
-Kazoku.Models.Family = Backbone.Model.extend({
+import Backbone from "backbone";
 
-  defaults: {
-    id: null,
-    name: "",
-    size: 0,
+
+class Person extends Backbone.Model {
+
+  get key() {
+    return this.get("id");
   }
-});
+
+  get defaults() {
+    return {
+      id: null,
+      name: "",
+      size: 0,
+    }
+  }
+
+  get urlRoot() {
+    return "/families/";
+  }
+
+  request(options={}) {
+    // var self = this;
+    options.success = function(model, response, options) {
+      console.log("request success:");
+      console.log(model);
+    }
+    options.error = function(model, response, options) {
+      console.log("request error:");
+      console.log(model);
+    }
+    var response = this.fetch(options);
+    return response;
+  }
+}
+
+
+module.exports = Family;

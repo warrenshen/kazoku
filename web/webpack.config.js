6199e68cb107857ad216e699847746c9ba7ea887
warren <warrenzshen@gmail.com>
2015-05-02 22:28:07 -0700

    Set up absolute paths without extensions

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -2,13 +2,14 @@ var webpack = require("webpack");
 
 module.exports = {
   entry: [
-    "webpack-dev-server/client?http://localhost:3333",
+    "webpack-dev-server/client?http://localhost:3333/",
     "webpack/hot/dev-server",
     __dirname + "/app/kazoku.js"
   ],
   output: {
     path: __dirname + "/build",
     filename: "bundle.js",
+    publicPath: "http://localhost:3333",
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
@@ -20,5 +21,9 @@ module.exports = {
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
       { test: /\.scss$/, exclude: /node_modules/, loader: "style!css!sass" },
     ]
-  }
+  },
+  resolve: {
+    root: __dirname,
+    extensions: ["", ".js", ".jsx"],
+  },
 }
